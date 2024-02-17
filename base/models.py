4557c79c10a41310c9cb34c0aeb51c3b1c0e095d
frank <ochoa.franco132@gmail.com>
2024-02-16 22:19:03 -0300

    uploading project

---
+++ base/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+# Create your models here.
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True) #it could be left null as well as being sent as a form null for the 2nd parameter.
+    #participants=
+    updated = models.DateTimeField(auto_now=True) # every time the saved method is called it takes a timestamp and save it here automatically.
+    created = models.DateTimeField(auto_now_add=True) # It only takes a timestamp when the instance is created (not every time is saved)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE) #One to many relationship, 1 room N messages.
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True) # every time the saved method is called it takes a timestamp and save it here automatically.
+    created = models.DateTimeField(auto_now_add=True) # It only takes a timestamp when the instance is created (not every time is saved)
+
+    def __str__(self) -> str:
+        return self.body[0:50] #trimming the message up to 50 characters.
+
---
+++ base/views.py
@@ -0,0 +1,123 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from django.db.models import Q 
+from .models import Room, Topic
+from .forms import RoomForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+# rooms = [
+#     {'id': 1, 'name': 'lets learn python'},
+#     {'id': 2, 'name': 'design with me'},
+#     {'id': 3, 'name': 'we hate frontend development'},
+
+# ]
+
+def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist')
+    context = {'page': page}
+    return render(request, 'base/login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+
+def registerPage(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST) #passing the data that was sent.
+        if form.is_valid():
+            user = form.save(commit=False) # with commit equal to false we can get the user object
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'an error ocurred during registration')
+    return render(request, 'base/login_register.html', {'form': form})
+
+
+def home(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q) 
+        )
+    
+    topics = Topic.objects.all()
+    room_count = rooms.count()    
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all() # getting all the messages related to this room.
+    context = {'room': room, 'room_messages': room_messages}
+    return render(request, 'base/room.html', context)
+
+@login_required(login_url='login')
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+@login_required(login_url='login')
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here!!')
+
+
+    if request.method =='POST':
+        form = RoomForm(request.POST, instance=room) #it's updating only this room instance
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+
+@login_required(login_url='login')
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here!!')
+    
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':room})
